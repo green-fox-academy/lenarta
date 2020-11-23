50199766791294f814e59327fcc48438b220dde2
András Lénárt <lenart.andraselod@gmail.com>
2020-11-23 21:08:34 +0100

    array exercises-02

---
+++ week-02/day-01/print-all.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+let numbers: number[] = [4, 5, 6, 7];
+for (let i: number = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
---
+++ week-02/day-01/swap-elements.ts
@@ -0,0 +1,11 @@
+'use strict'
+
+let names: string[] = ['Arthur', 'Boe', 'Chloe'];
+names.reverse();
+/*
+let change: string = '';
+change = names[0];
+names[0] = names[2];
+names[2] = change;
+*/
+console.log(names);
