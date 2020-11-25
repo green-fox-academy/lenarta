f1fcf259e1945474d3ffab2183b0579a57b38ee4
András Lénárt <lenart.andraselod@gmail.com>
2020-11-25 07:08:48 +0100

    end of day-02

---
+++ week-02/day-02/listIntroduction-01.ts
@@ -0,0 +1,37 @@
+'use strict';
+
+let names1: string[] = [];
+console.log(names1.length);
+
+names1.push('William');
+
+if (names1.length !== 0) {
+    console.log('The list isn\'t empty')
+} else console.log('The list is empty')
+
+
+names1.push('John', 'Amanda');
+
+console.log(names1.length);
+console.log(names1[2]);
+
+for (let i: number = 0; i < names1.length; i++) {
+    console.log(names1[i]);
+}
+
+for (let i: number = 0; i < names1.length; i++) {
+    console.log((i + 1) + '. ' + names1[i]);
+}
+
+names1.splice(1, 1);
+
+for (let i: number = names1.length - 1; i >= 0; i--) {
+    console.log(names1[i])
+}
+names1.splice(0, 2);
+
+if (names1.length !== 0) {
+    console.log('The list isn\'t empty')
+} else console.log('The list is empty')
+
+
