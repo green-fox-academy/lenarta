68e3a88781b610da4cf4dd1cab3d1f5a2502c9dd
András Lénárt <lenart.andraselod@gmail.com>
2020-12-03 11:31:24 +0100

    oop pokemon

---
+++ week-03/day-03/counter.ts
@@ -0,0 +1,43 @@
+'use strict';
+
+export class Counter {
+
+    private _integer: number;
+    private _initial: number;
+
+    constructor(initial = 0) {
+
+        this._integer = initial;
+        this._initial = initial;
+    }
+
+    public add(number?): void {
+        if (number) {
+            this._integer += number;
+        } else {
+            this._integer++;
+        }
+    }
+
+    public get(): number {
+        return this._integer;
+    }
+
+    public reset(): void {
+        this._integer = this._initial;
+    }
+}
+
+
+let x = new Counter();
+
+x.add(5);
+console.log(x);
+
+x.get();
+console.log(x);
+
+x.reset();
+console.log(x);
+
+
