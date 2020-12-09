164b6889518de4fb7eeff56e91081bce457175b5
András Lénárt <lenart.andraselod@gmail.com>
2020-12-09 19:21:31 +0100

    testing exercises

--- week-04/day-03/testingPractice/countLetters.ts
+++ week-04/day-03/testingPractice/countLetters.ts
@@ -2,4 +2,16 @@
 
 export function countLetters(text: string) {
     let dictionary = {}
+
+    for (let i: number = 0; i < text.length; i++) {
+
+        if (text.charAt(i) in dictionary) {
+            dictionary[text.charAt(i)] += 1;
+        } else {
+            dictionary[text.charAt(i)] = 1;
+        }
+    }
+    return dictionary
 }
+
+console.log(countLetters('saaajooosstttettakok'));
--- week-04/day-03/testingPractice/sum.test.ts
+++ week-04/day-03/testingPractice/sum.test.ts
@@ -4,9 +4,31 @@ import { Sum } from './sum';
 test('should summarise', t => {
     const operations = new Sum();
 
-    const actual = operations.sum([1, 2]);
-    const expected = 3;
+    const actual = operations.sum([1, 2, 3, 4, 5]);
+    const expected = 15;
 
     t.equal(actual, expected);
     t.end();
 });
+
+test('should summarise', t => {
+    const operations = new Sum();
+
+    const actual = operations.sum([]);
+    const expected = 0;
+
+    t.equal(actual, expected);
+    t.end();
+});
+
+test('should summarise', t => {
+    const operations = new Sum();
+
+    const actual = operations.sum([98]);
+    const expected = 98;
+
+    t.equal(actual, expected);
+    t.end();
+});
+
+
