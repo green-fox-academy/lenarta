15ce6607b9b1c3d4b941e645c0bbec6c3d2209c1
András Lénárt <lenart.andraselod@gmail.com>
2021-01-11 16:52:34 +0100

    Gallery buttons add

--- week-06/day-05/GalleryProject/gallery.js
+++ week-06/day-05/GalleryProject/gallery.js
@@ -1,22 +1,26 @@
 'use strict';
 
 const pictures = [
- {title: '' , path:'assets/pic01.jpg'},
- {title: '' , path: 'assets/pic02.jpg'},
- {title: 'Nebula' , path:'assets/pic03.jpg'},
- {title: 'Pillars' , path: 'assets/pic04.jpg'},
- {title: '' , path: 'assets/pic05.jpg'},
- {title: '' , path: 'assets/pic06.jpg'},
- {title: '' , path: 'assets/pic07.jpg'},
- {title: '' , path:'assets/pic08.jpg'},
-]
+  { title: '', path: 'assets/pic01.jpg' },
+  { title: '', path: 'assets/pic02.jpg' },
+  { title: 'Nebula', path: 'assets/pic03.jpg' },
+  { title: 'Pillars', path: 'assets/pic04.jpg' },
+  { title: '', path: 'assets/pic05.jpg' },
+  { title: '', path: 'assets/pic06.jpg' },
+  { title: '', path: 'assets/pic07.jpg' },
+  { title: '', path: 'assets/pic08.jpg' },
+];
 
 const mainPic = document.querySelector('img');
-console.log(mainPic.classList);
+console.log(Object.values(pictures));
 
 const picturebox = document.querySelector('.picturebox');
+let currentPic = 0;
 
-picturebox.setAttribute('style', 'background-image: url("assets/pic01.jpg");');
+picturebox.setAttribute(
+  'style',
+  `background-image: url("${pictures[currentPic].path}");`
+);
 
 let thumbNailList = document.querySelector('.thumbnails');
 
@@ -24,15 +28,49 @@ console.log(thumbNailList);
 
 for (let i = 0; i < pictures.length; i++) {
   let newPic = document.createElement('li');
-  newPic.classList.add('pic')
-  newPic.id = `pic0${i+1}`;
-  console.log(newPic)
+  newPic.classList.add('pic');
+  newPic.id = `pic0${i + 1}`;
+  console.log(newPic);
   thumbNailList.appendChild(newPic);
-  newPic.setAttribute('style', `background-image: url("assets/pic0${i+1}.jpg");`);
+  newPic.setAttribute(
+    'style',
+    `background-image: url("${pictures[i].path}");`
+  );
 }
 
+const rightButton = document.getElementById('right');
+rightButton.onclick = () => {
+  picturebox.setAttribute(
+    'style',
+    `background-image: url("${pictures[currentPic + 1].path}");`
+  );
 
+  let counter = currentPic + 1;
+  counter++;
+  currentPic++;
 
-button.onclick = () => {
-  
+  if (counter === pictures.length) {
+    currentPic = -1;
+  }
+};
+
+const leftButton = document.getElementById('left');
+leftButton.onclick = () => {
+  let counter = currentPic + 1;
+
+  if (counter === 1) {
+    picturebox.setAttribute(
+      'style',
+      `background-image: url("${pictures[pictures.length - 1].path}");`
+    );
+    currentPic = pictures.length - 1;
+    console.log(counter, currentPic);
+  } else {
+    picturebox.setAttribute(
+      'style',
+      `background-image: url("${pictures[currentPic - 1].path}");`
+    );
+    counter--;
+    currentPic--;
+  }
 };
