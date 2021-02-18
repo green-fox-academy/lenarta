b5a636855edff7bf249af912e71b8bdf30579d68
András Lénárt <lenart.andraselod@gmail.com>
2021-02-18 19:56:47 +0100

    paginator buttons and CSS work

--- project00/my-app/src/App.js
+++ project00/my-app/src/App.js
@@ -6,6 +6,9 @@ function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [beers, setBeers] = useState([]);
 
+  const [renderStart, setrenderStart] = useState(0);
+  const [renderEnd, setrenderEnd] = useState(7);
+
   useEffect(() => {
     fetch('https://api.punkapi.com/v2/beers')
       .then((response) => response.json())
@@ -21,12 +24,25 @@ function App() {
       );
   }, []);
 
-  const [renderStart, setrenderStart] = useState(0);
-  const [renderEnd, setrenderEnd] = useState(7);
+  //const [renderedBeers, setRenderedBeers] = useState([]);
 
   const renderedBeers = beers.filter((beer) => {
     return beer.id > renderStart && beer.id < renderEnd;
   });
+
+  const handleClickNext = () => {
+    setrenderStart(renderStart + 6);
+    setrenderEnd(renderEnd + 6);
+  };
+  const handleClickPrev = () => {
+    setrenderStart(renderStart - 6);
+    setrenderEnd(renderEnd - 6);
+  };
+  const handleClickPic = (id) => {
+    console.log(beers[id].description);
+  };
+
+  const previousButton = renderEnd > 7;
 
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -39,10 +55,24 @@ function App() {
         <div className="content">
           {renderedBeers.map((beer) => (
             <div className="beers" key={beer.id}>
-              <img src={beer.image_url}></img>
+              <img
+                src={beer.image_url}
+                onClick={() => handleClickPic(beer.id)}
+              ></img>
               <h1>{beer.name}</h1>
             </div>
           ))}
+        </div>
+        <div className="paginator">
+          {previousButton && (
+            <button id="previous" onClick={handleClickPrev}>
+              Previous
+            </button>
+          )}
+
+          <button id="next" onClick={handleClickNext}>
+            Next
+          </button>
         </div>
       </div>
     );
